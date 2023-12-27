6618e35f5ae323fcb6a505b67c4456ef7a03608f
HilmiStd <2205040027@student.unimma.ac.id>
2023-12-28 00:13:33 +0700

    menambahkan modal sukses ketika user berhasil diagnosa

--- app/Views/user/header.php
+++ app/Views/user/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?php echo base_url('asset/dist');?>/img/sikarpar.svg" >
     <title>Diagnosa Pasien</title>
     <style type="text/css" id="debugbar_dynamic_style"></style>
     <link rel="stylesheet" href="<?php echo base_url('asset/dist'); ?>/css/adminlte.min.css">
